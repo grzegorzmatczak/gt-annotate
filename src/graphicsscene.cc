4d3790cc8c3fbd19fbbb342f17b83718196191c0
Grzegorz Matczak <grzegorz.matczak@unikie.com>
2021-05-06 21:25:57 +0200

    Add initial graphic view

---
+++ src/graphicsscene.cc
@@ -0,0 +1,152 @@
+#include "graphicsscene.h"
+
+#ifndef QT_NO_OPENGL
+#include <QtOpenGL>
+#else
+#include <QtWidgets>
+#endif
+#include <qmath.h>
+
+GraphicsScene::GraphicsScene()
+	: QGraphicsScene()
+	, m_justSelect(false)
+	, m_mode(uiMode::None)
+{}
+
+QRectF GraphicsScene::transformPos(QRectF pos)
+{
+	return pos;
+}
+
+void GraphicsScene::mousePressEvent(QGraphicsSceneMouseEvent* e)
+{
+	if (m_mode == uiMode::Paint && e->buttons() == Qt::LeftButton)
+	{
+		qreal xreal = e->scenePos().x();
+		qreal yreal = e->scenePos().y();
+		onPaintWhiteBoard(xreal, yreal);
+		emit(updateView());
+	}
+	else if (m_mode == uiMode::SelectROI && e->buttons() == Qt::LeftButton)
+	{
+		m_startPoint = e->scenePos(); // Save start pos to save ROI on scene.
+		setSelectionArea(QPainterPath());
+		emit(updateView());
+		m_justSelect = false;
+	}
+	if (m_mode == uiMode::SelectROI && e->buttons() == Qt::MiddleButton)
+	{
+		m_justSelect = true;
+	}
+	else if (m_mode == uiMode::MoveSelect && e->buttons() == Qt::LeftButton)
+	{
+		qreal xreal = e->scenePos().rx();
+		qreal yreal = e->scenePos().ry();
+		int x = static_cast<int>(xreal);
+		int y = static_cast<int>(yreal);
+		QPointF point = QPointF(x, y);
+		e->setScenePos(point);
+		QGraphicsScene::mousePressEvent(e);
+	}
+}
+
+void GraphicsScene::onResetScene()
+{
+	QList<QGraphicsItem*> all = items();
+	for (int i = 0; i < all.size(); i++)
+	{
+		QGraphicsItem* gi = all[i];
+		if (gi->parentItem() == NULL)
+		{
+			delete gi;
+		}
+	}
+}
+
+void GraphicsScene::mouseMoveEvent(QGraphicsSceneMouseEvent* e)
+{
+	if (m_mode == uiMode::Paint && e->buttons() == Qt::LeftButton)
+	{
+		qreal xreal = e->scenePos().rx();
+		qreal yreal = e->scenePos().ry();
+		onPaintWhiteBoard(xreal, yreal);
+	}
+	else if (m_mode == uiMode::SelectROI)
+	{
+		QGraphicsScene::mouseMoveEvent(e);
+		emit(updateView());
+	}
+	else if (m_mode == uiMode::MoveSelect)
+	{
+		QGraphicsScene::mouseMoveEvent(e);
+	}
+}
+
+void GraphicsScene::onPaintWhiteBoard(qreal x, qreal y)
+{
+	int rx = static_cast<int>(x);
+	int ry = static_cast<int>(y);
+	emit(paintWhiteBoard(rx, ry));
+}
+
+void GraphicsScene::mouseReleaseEvent(QGraphicsSceneMouseEvent* e)
+{
+	if (m_mode == uiMode::Paint)
+	{
+	}
+	else if (m_mode == uiMode::SelectROI && !m_justSelect)
+	{
+		QPainterPath tmpPath = selectionArea();
+		setSelectionArea(QPainterPath());
+
+		if (tmpPath.isEmpty())
+		{
+		}
+		else
+		{
+			emit(addRectToScene(m_startPoint, e->scenePos(), true, ""));
+			emit(updateView());
+		}
+
+		QGraphicsScene::mouseReleaseEvent(e);
+	}
+	else if (m_mode == uiMode::MoveSelect)
+	{
+		QGraphicsScene::mouseReleaseEvent(e);
+	}
+}
+
+void GraphicsScene::onSetModeROI()
+{
+	m_mode = uiMode::SelectROI;
+}
+
+void GraphicsScene::onSetModePaint()
+{
+	m_mode = uiMode::Paint;
+}
+
+void GraphicsScene::onSetModeMoveSelect()
+{
+	m_mode = uiMode::MoveSelect;
+}
+
+void GraphicsScene::keyPressEvent(QKeyEvent* event)
+{
+	if (event->key() == Qt::Key_Delete)
+	{
+		if (selectedItems().count() > 0)
+		{
+            /*
+			QGraphicsItem* itemToRemove = selectedItems()[0];
+			if (itemToRemove->type() == 2)
+			{
+				SelectText* cast = dynamic_cast<SelectText*>(itemToRemove);
+				emit(deleteItem(cast->id()));
+			}
+			removeItem(itemToRemove);
+			delete itemToRemove;*/
+		}
+	}
+	QGraphicsScene::keyPressEvent(event);
+}
---
+++ src/graphicsview.cc
@@ -0,0 +1,82 @@
+#include "graphicsview.h"
+
+#ifndef QT_NO_OPENGL
+#include <QtOpenGL>
+#else
+#include <QtWidgets>
+#endif
+#include <qmath.h>
+
+class View;
+#define DEBUG FALSE
+
+GraphicsView::GraphicsView()
+	: QGraphicsView()
+	, m_mode(0)
+{}
+
+void GraphicsView::wheelEvent(QWheelEvent* e)
+{
+#if(DEBUG)
+	H_Logger->trace("GraphicsView::wheelEvent()");
+#endif
+	if (e->modifiers() & Qt::ControlModifier)
+	{
+		if (e->delta() > 0)
+			emit(zoomIn(22));
+		else
+			emit(zoomOut(22));
+
+		e->accept();
+	}
+	else
+	{
+		QGraphicsView::wheelEvent(e);
+	}
+}
+
+bool GraphicsView::drawing()
+{
+#if(DEBUG)
+	H_Logger->trace("GraphicsView::drawing()");
+#endif
+	if (m_mode == uiMode::Paint)
+		return true;
+	else
+		return false;
+}
+
+bool GraphicsView::editing()
+{
+#if(DEBUG)
+	H_Logger->trace("GraphicsView::editing()");
+#endif
+	if (m_mode == uiMode::SelectROI)
+		return true;
+	else
+		return false;
+}
+
+void GraphicsView::mousePressEvent(QMouseEvent* e)
+{
+#if(DEBUG)
+	H_Logger->trace("GraphicsView::mousePressEvent()");
+#endif
+	m_initPos = e->pos();
+#if(DEBUG)
+	qDebug() << "GraphicsView::mousePressEvent" << e->pos();
+#endif
+	if (e->buttons() == Qt::MiddleButton)
+	{
+	}
+	this->repaint();
+	QGraphicsView::mousePressEvent(e);
+}
+
+void GraphicsView::mouseMoveEvent(QMouseEvent* event)
+{
+#if(DEBUG)
+	H_Logger->trace("GraphicsView::mouseMoveEvent()")
+#endif
+	QGraphicsView::mouseMoveEvent(event);
+}
