f9f858dbefa30fdfefec4975c4c658b43d4bcb83
Grzegorz Matczak <grzegorz.matczak@unikie.com>
2021-05-14 20:55:50 +0200

    Add initial paintersetting implementation

--- include/widgets/paintersettings.cc
+++ include/widgets/paintersettings.cc
@@ -4,3 +4,25 @@ PainterSettings::PainterSettings()
 {
 
 }
+
+void PainterSettings::configureColors(QJsonObject const& a_config)
+{
+    Logger->trace("PainterSettings::configureColors()");
+    auto _colors = a_config[COLORS].toArray();
+    for (int i = 0; i < _colors.size(); i++)
+    {
+        auto _colorsIter = _colors[i].toObject();
+        QString _name = _colorsIter[NAME].toString();
+        m_colors.push_back(_name);
+        int _r = _colorsIter[R].toInt();
+        int _g = _colorsIter[G].toInt();
+        int _b = _colorsIter[B].toInt();
+        int _a = _colorsIter[A].toInt();
+        int _gray = _colorsIter[GRAY_COLOR].toInt();
+
+        m_colorInthash.insert(_name, _gray);
+        m_colorHash.insert(_name, { _r, _g, _b, _a });
+    }
+    qDebug() << "m_colorInthash:" << m_colorInthash;
+    qDebug() << "m_colorHash:" << m_colorHash;
+}
--- include/widgets/paintersettings.h
+++ include/widgets/paintersettings.h
@@ -6,8 +6,19 @@
 class PainterSettings
 {
     Q_OBJECT
-public:
-    PainterSettings();
+    public:
+        PainterSettings();
+        void configureColors(QJsonObject const& a_config);
+
+    private:
+        std::vector<QString> m_colors;
+        qint32 m_penSize{};
+        QColor m_color;
+
+    private:
+        QHash<QString, QColor> m_colorHash;
+        QHash<QString, int> m_colorInthash;
 };
+
 
 #endif // PAINTERSETTINGS_H
